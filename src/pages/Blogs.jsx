a478fa7b5acddef610d23e66153d3c7cbe665183
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-06-30 06:35:01 +0530

    added blogs content , styled scrollbar

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -9,14 +9,27 @@ function Blogs() {
     return (
         <AnimatePresence>
             <motion.div
-               initial="initial"
-               animate="animate"
-               exit="exit"
-               variants={variants}
-               className="view-container mt-6 w-[900px] grid grid-col-1 item-start justify-start gap-12"
-               >
-                blgs
-                
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={variants}
+                className="view-container mt-[50px] mb-[150px] w-[900px] grid grid-col-1 item-start justify-start gap-6"
+            >
+                <div className='font-bold text-3xl mb-5'>Blogs</div>
+                <div class=" rounded-xl bg-zinc-700  pb-6 overflow-hidden shadow-lg hover:scale-105 transition">
+                    <img class="w-full" src="./Thumbnail.jpg" alt="Article Image" />
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">TRAVEL CREW - A Flutter App Built with Appwrite | Appwrite Hashnode Hackathon</div>
+                        <p class="text-zinc-400 text-lg font-normal">Planning your next trip, but can't find travel partners? We solved your Problem!</p>
+                    </div>
+                    <div class="px-6 py-4">
+                        <a href="https://hashnode.com/article-url" class="bg-zinc-500 hover:bg-zinc-900 transition text-white font-bold py-2 px-4 rounded-full">
+                            Read Article
+                        </a>
+                    </div>
+                </div>
+
+
             </motion.div>
         </AnimatePresence>
     )
