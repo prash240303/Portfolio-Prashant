9a6b1fa3006d654d5c723bf3bc12bb3c12a9fec0
prash240303 <prashantp.cs.21@nitj.ac.in>
2023-10-31 14:05:32 +0530

    optimisation : image size

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,9 +5,9 @@ const ProjectCard = ({ title, year, description, image, githubUrl }) => {
     <a href={githubUrl} target="_blank" rel="noreferrer">
       <div className="p-5 mb-3 flex flex-col items-center justify-center cursor-pointer rounded-3xl custom-gradient transition-all ease-in-out">
         <div className="image-container rounded-xl">
-          <img src={image} className="rounded-xl w-fill h-fill" alt="project image" />
+          <img src={image} className="rounded-xl w-[213px] h-[140px]" alt="project image" />
         </div>
-        <div className="label w-fill flex flex-col items-start justify-start mt-[6px]">
+        <div className="label w-full flex flex-col items-start justify-start mt-[6px]">
           <div className="text-lg font-semibold my-1">{title}</div>
           <div className="text-sm opacity-30 leading-tight md:mb-0 mb-3">{year}</div>
           <div className="text-[15px] mt-2 w-fit font-light opacity-60 leading-snug">
