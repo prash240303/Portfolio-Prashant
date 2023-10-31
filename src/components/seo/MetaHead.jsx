fd31751ad9214e850faef1043886d1e856ea2a5a
prash240303 <prashantp.cs.21@nitj.ac.in>
2023-10-31 19:52:30 +0530

    Optimisation : SEO added MetaHead

--- src/components/seo/MetaHead.jsx
+++ src/components/seo/MetaHead.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import Head from 'next/head';
+import PropTypes from 'prop-types';
 
 const MetaHead = ({
   title = 'Prashant',
-  description = 'Hey! I am a Software Engineer. I love developing stuff that makes people\'s lives easier.',
+  description = "Hey! I am a Software Engineer. I love developing stuff that makes people's lives easier.",
   embedSource = 'default',
   ...attr
 }) => {
   return (
-    <Head {...attr}>
+    <head {...attr}>
       <title>{title}</title>
       <link rel="shortcut icon" href="/media/icon.ico" />
       <meta name="title" content={title} />
@@ -35,7 +34,7 @@ const MetaHead = ({
             : 'summary' // Update with appropriate twitter card type
         }
       />
-      <meta property="twitter:url" content="https://www.thealonemusk.com/" />
+      <meta property="twitter:url" content="https://portfolio-prash.vercel.app/" />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta
@@ -46,8 +45,20 @@ const MetaHead = ({
             : embedSource?.twitter
         }
       />
-    </Head>
+    </head>
   );
+};
+
+MetaHead.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  embedSource: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      og: PropTypes.string,
+      twitter: PropTypes.string,
+    }),
+  ]),
 };
 
 export default MetaHead;
