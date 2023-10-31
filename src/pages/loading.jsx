76a723795892445ac0a38683c7ebf5361459f98c
prash240303 <prashantp.cs.21@nitj.ac.in>
2023-10-31 14:08:41 +0530

    optimisation : changed loading animation to mp4

--- src/pages/loading.jsx
+++ src/pages/loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="loading flex items-center justify-center h-screen">
-        <img src="./icons/noto_sparkles.gif" className='w-24 aspect-auto' alt="loading gif" />
+        <img src="./icons/noto_sparkles.mp4" className='w-24 aspect-auto' alt="loading gif" />
     </div>
   );
 };
