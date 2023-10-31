e3b6ff53189fe2aeeac90e422e78a89c5981156d
prash240303 <prashantp.cs.21@nitj.ac.in>
2023-10-31 14:10:40 +0530

    fix: image tag changed to video

--- src/pages/loading.jsx
+++ src/pages/loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="loading flex items-center justify-center h-screen">
-        <img src="./icons/noto_sparkles.mp4" className='w-24 aspect-auto' alt="loading gif" />
+        <video src="./icons/noto_sparkles.mp4" className='w-24 aspect-auto' alt="loading gif" />
     </div>
   );
 };
