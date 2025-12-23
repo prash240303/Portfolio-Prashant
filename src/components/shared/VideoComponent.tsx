9ac46b247254fc3a08e1abbc9e3d1e13d4d58ccb
prashant <prash2402@gmail.com>
2025-12-24 00:21:57 +0530

    fix(bui ld) - projects vid

--- src/components/shared/VideoComponent.tsx
+++ src/components/shared/VideoComponent.tsx
@@ -1,4 +1,3 @@
-import { list } from "@vercel/blob";
 async function VideoComponent({
   fileName,
   title,
@@ -6,12 +5,6 @@ async function VideoComponent({
   fileName: string;
   title: string;
 }) {
-  const { blobs } = await list({
-    prefix: fileName,
-    limit: 1,
-  });
-  const url =
-    "https://pasp3hha2jfsaiwz.public.blob.vercel-storage.com/echo-chat-AB1zhCP8soPOmnJJP7Fho36VpJgaFg.mp4";
   return (
     <video
       className="size-full rounded-lg object-cover transition-all duration-300 hover:scale-105"
@@ -23,7 +16,7 @@ async function VideoComponent({
       controls={false}
       preload="none"
     >
-      <source src={url} type="video/mp4" />
+      <source src={fileName} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
